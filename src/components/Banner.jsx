5c79b54d7a92f7e5f74e32d35e078bd073125fd1
khaled6013 <kmd601384@gmail.com>
2025-12-19 19:59:35 +0600

    img done

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import Img from '../assets/serve.jpg'
 
 const Banner = () => {
   return (
-    <div>Banner</div>
+    <>
+       <section className='py-20'>
+        <div className="w-10/12 mx-auto">
+           <div className="bg-gray-500 rounded-lg p-10">
+             <img src={Img} alt="" className='w-75'/>
+           </div>
+        </div>
+       </section>
+    </>
   )
 }
 
